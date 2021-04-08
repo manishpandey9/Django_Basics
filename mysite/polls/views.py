47f107804b1a9ce89e22b047dd6e9b1109927a93
Manish <manish.p@healthify.co>
2021-04-09 03:55:02 +0530

    added views to the poll app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from .models import Question
 from django.http import HttpResponse 
 
 def index(request):
@@ -9,7 +10,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -18,6 +23,17 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
