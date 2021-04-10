e8236ee403c46eca934b0cf517addb9a06f3b7d8
Manish <manish.p@healthify.co>
2021-04-10 20:26:39 +0530

    added the views and template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Question
-from django.http import HttpResponse 
+from django.http import HttpResponse, HttpResponseRedirect 
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -10,16 +13,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+     question = get_object_or_404(Question, pk=question_id)
+     return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
